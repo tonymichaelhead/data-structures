a991e4b5b88809f5f9bb35e69e36e1b755628e4a
tonymichaelhead <tonymichaelhead@gmail.com>
2017-06-28 14:20:43 -0700

    Implement Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,30 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    //loop over existing props and assign them new key names (+1)
+    storage[size] = value;
+    size ++;
   };
 
   someInstance.dequeue = function() {
+     
+    if (size === 0 ) { return null; }
+    var dequeued = storage['0'];
+    delete storage['0'];
+    for (var key in storage) {
+      storage[Number(key) - 1] = storage[key];
+    }
+    size--;
+    return dequeued;
   };
 
   someInstance.size = function() {
+    return size;
   };
 
   return someInstance;
