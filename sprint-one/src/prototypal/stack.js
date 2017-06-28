b493ab6fe9da571f585422c3e2a87976ceddc785
tonymichaelhead <tonymichaelhead@gmail.com>
2017-06-28 16:09:08 -0700

    Implement protoptypal class

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var instance = Object.create(stackMethods);
+  instance.length = 0;
+  instance.storage = {};
+
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.length] = value;
+    this.length++;
+  },
+  pop: function() {
+    if (this.length === 0) { return null; }
+
+    var popped = this.storage[this.length - 1];
+    delete this.storage[this.length - 1];
+    this.length--;
+    return popped;  
+  },
+  size: function() {
+    return this.length;
+  }
+};
 
 
