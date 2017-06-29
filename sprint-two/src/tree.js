d99f10453bfc0d84bf654f18230233a59024779f
tonymichaelhead <tonymichaelhead@gmail.com>
2017-06-29 16:31:36 -0700

    Removed unecessary if-statement from Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,8 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
@@ -11,13 +10,33 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  var currentNode = this;
+  var checker = false;
+
+  var search = function(node) {
+    if (node.value === target) {
+      checker = true;
+    } else {
+      if (node.children) {
+        node.children.forEach(function(child) {
+          search(child);
+        });
+      } 
+    }
+  };
+  search(currentNode);
+  
+  return checker;
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ addChild = O(1) - Constant
+ contains = O(n) - Linear
  */
