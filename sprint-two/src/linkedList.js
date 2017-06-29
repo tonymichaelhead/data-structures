7489068ad5be12b7e166988ba0edc908f6e30488
tonymichaelhead <tonymichaelhead@gmail.com>
2017-06-29 14:22:45 -0700

    Implement linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,50 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (list.tail === null) {
+      list.tail = Node(value);
+      list.head = list.tail;
+    } else {
+      var previousTail = list.tail;
+      previousTail.next = Node(value);
+      list.tail = previousTail.next;
+    }
+    //instansiate new node
+    //change old tail node to now point to the new tail
+    
+    //point tail to new node value  
   };
 
   list.removeHead = function() {
+    //point head to new head(list.head.next)
+    var currentHead = list.head;
+    //list.head.next = null;
+    list.head = list.head.next;
+
+    return currentHead.value;
+    //remove reference from old head and 
+    // if (list.head.next === null) {
+    //   list.head = null;
+    // } else {
+
+    //   list.head = list.head.next;
+
+    // }
+    //return stored head value
   };
 
   list.contains = function(target) {
+    var checkNodes = function(node) {
+      if (node.value === target) {
+        return true;
+      }
+      if (node.next) {
+        return checkNodes(node.next);
+      }
+      return false;
+    };
+    return checkNodes(list.head);
   };
-
   return list;
 };
 
